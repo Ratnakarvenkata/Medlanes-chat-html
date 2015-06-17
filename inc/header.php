e28c5b815bd6f686be7ba649efca29d3f1eafc24
Ratnakar mv <ratnakarmv@yahoo.com>
2015-06-17 17:29:50 +0530

    Medlanes Chat Frontend

---
+++ inc/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Chat</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+
+   <!-- Mobile Specific Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+    ================================================== -->
+    
+   <link rel="stylesheet" href="css/base.css">
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/responsive/media-queries.css">
+   <link rel="stylesheet" href="css/animate/animate.css">        
+
+   <!-- Script
+   =================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	=================================================== -->
+	<link rel="shortcut icon" href="favicon.png" >
+
+</head>
+
+<body class="homepage">
+   <!-- Header
+   =================================================== -->
+   <header id="main-header">
+
+   	<div class="row header-inner">
+
+	      <div class="logo">
+	         <a class="smoothscroll" href="#hero">Logo</a>
+	      </div>
+
+	      <nav id="nav-wrap" class="pull-right">         
+	         
+	         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">
+	         	<span class='menu-text'>Show Menu</span>
+	         	<span class="menu-icon"></span>
+	         </a>
+         	<a class="mobile-btn" href="#" title="Hide navigation">
+         		<span class='menu-text'>Hide Menu</span>
+         		<span class="menu-icon"></span>
+         	</a>         
+
+	         <ul id="nav" class="nav">
+		         <li><a class="smoothscroll" href="#about">About</a></li>
+	            <li><a class="smoothscroll" href="#doctors">Doctors</a></li>
+	            <li><a class="smoothscroll" href="#support">Support</a></li>
+	         </ul> 
+
+	      </nav> <!-- /nav-wrap -->	      
+
+	   </div> <!-- /header-inner -->
+
+   </header> 
---
+++ medllanes-GmbH/inc/footer.php
@@ -0,0 +1,38 @@
+   <!-- Footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">       
+
+         <div id="go-top">
+            <a class="smoothscroll" title="Back to Top" href="#hero"><span>Top</span><i class="fa fa-long-arrow-up"></i></a>
+         </div>
+		<ul>
+        	<li><a href="#">Home</a></li>
+            <li><a href="#">Terms &amp; Conditions</a></li>
+            <li><a href="#">Privacy Statement</a></li>
+            <li><a href="#">Contact</a></li>	
+        </ul>
+      </div> <!-- /row -->
+
+   </footer> <!-- /footer -->
+
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/global/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/global/jquery-migrate-1.2.1.min.js"></script>   
+   <script src="js/plugins/flexslider/jquery.flexslider.js"></script>
+   <script src="js/plugins/fittext/jquery.fittext.js"></script>
+   <script src="js/plugins/backstretch/backstretch.js"></script> 
+   <script src="js/plugins/waypoints/waypoints.js"></script> 
+   <script src="js/global/main.js"></script>
+   <script src="js/plugins/wow/wow.min.js"></script> 
+   <script>
+       new WOW().init();
+   </script>
+  
+</body>
+
+</html>
